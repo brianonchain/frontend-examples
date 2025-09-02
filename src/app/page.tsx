d2c999e8b2709b52610497f4aa9f0201d94e1bbf
brianonchain <jonwayhuang@gmail.com>
2025-09-02 15:16:52 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import Link from "next/link";
 
 export default function Landing() {
   return (
-    <div className="py-20 flex justify-center">
+    <div className="pb-20 flex justify-center bg-black text-white">
       <div className="landingPageWidth">
-        <div className="w-full flex justify-evenly">
+        <p className="text-center text-3xl sm:text-5xl md:text-6xl font-bold leading-[1.2] py-10 md:py-16">Website Mockups Showcasing Animation Techniques</p>
+        <div className="w-full flex flex-col items-center lg:flex-row lg:items-stretch lg:justify-evenly gap-12 md:gap-16">
           <Link
             href="/github"
             target="_blank"
-            className="w-[348px] h-[520px] border border-slate-400 rounded-3xl overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 cursor-pointer"
+            className="w-[348px] bg-slate-900 border-2 border-slate-700 rounded-2xl overflow-hidden hover:shadow-[0px_0px_40px_10px_#373D63] transition-all duration-500 cursor-pointer"
           >
             <Image src="/github.png" alt="Github" width={2527} height={1290} className="w-full h-[200px] object-cover border-b border-slate-400" />
             <div className="p-3">
-              <p className="font-bold text-center">Github Mockup</p>
+              <p className="font-bold text-center text-2xl mb-4">www.github.com/home</p>
               <p>Features:</p>
               <div className="space-y-1">
                 <p className="flex items-start gap-2">
@@ -50,11 +51,11 @@ export default function Landing() {
           <Link
             href="/stripe"
             target="_blank"
-            className="w-[348px] h-[520px] border border-slate-400 rounded-3xl overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 cursor-pointer"
+            className="w-[348px] bg-slate-900 border-2 border-slate-700 rounded-2xl overflow-hidden hover:shadow-[0px_0px_40px_10px_#373D63] transition-all duration-500 cursor-pointer"
           >
             <Image src="/stripe.png" alt="Github" width={2521} height={1295} className="w-full h-[200px] object-cover border-b border-slate-400" />
             <div className="p-3">
-              <p className="font-bold text-center">Stripe Mockup</p>
+              <p className="font-bold text-center text-2xl mb-4">www.stripe.com</p>
               <p>Features:</p>
               <div className="space-y-1">
                 <p className="flex items-start gap-2">
